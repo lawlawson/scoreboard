ae5b4975660ff333eef2409a3f30521b1c051a52
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-10-27 18:38:14 +0000

    added functionality to increment scores

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('Working');
+let count = 0;
+let countScore = document.getElementById('home-score');
+
+function plusOneHome() {
+  count += 1;
+  countScore.textContent = count;
+}
+
+function plusTwoHome() {
+  count += 2;
+  countScore.textContent = count;
+}
+
+function plusThreeHome() {
+  count += 3;
+  countScore.textContent = count;
+}
+
+function plusOneGuest() {
+  count += 1;
+  countScore.textContent = count;
+}
+
+function plusTwoGuest() {
+  count += 2;
+  countScore.textContent = count;
+}
+
+function plusThreeGuest() {
+  count += 3;
+  countScore.textContent = count;
+}
